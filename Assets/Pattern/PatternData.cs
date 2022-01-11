baba5c0fc0929788f1cac545aad25ee6fffade42
roninflame <39270713+bsancan@users.noreply.github.com>
2022-01-11 09:51:47 -0500

    data - plugins

---
+++ Assets/Pattern/PatternData.cs
@@ -0,0 +1,44 @@
+﻿using PolloScripts.Enums;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PolloScripts
+{
+    public class PatternData
+    {
+        //public AssetType assetType = AssetType.None;
+        public EnemyType enemyTpe = EnemyType.None;
+        public ItemType itemType = ItemType.None;
+        public int typeID;
+        public Vector3 Position;
+        public Quaternion Rotation;
+        public int trayectoria;
+
+        public void SetData(ItemType asset, int typeID, Vector3 position, Quaternion rotation)
+        {
+            itemType = asset;
+            this.typeID = typeID;
+            Position = position;
+            Rotation = rotation;
+        }
+
+        public void SetData(EnemyType asset, int typeID, Vector3 position, Quaternion rotation)
+        {
+            enemyTpe = asset;
+            this.typeID = typeID;
+            Position = position;
+            Rotation = rotation;
+        }
+
+        public void SetData(ItemType asset, int typeID, int trayectoria, Vector3 position, Quaternion rotation)
+        {
+            itemType = asset;
+            this.typeID = typeID;
+            Position = position;
+            Rotation = rotation;
+            this.trayectoria = trayectoria;
+        }
+    }
+}
+
---
+++ Assets/Pattern/PatternManager.cs
@@ -0,0 +1,68 @@
+﻿using PolloScripts.Enums;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PolloScripts
+{
+    public class PatternManager : MonoBehaviour
+    {
+        [SerializeField] List<GameObject> levelPatterns;
+        public List<List<PatternData>> LevelPatternData;
+        public void Initialize()
+        {
+            LevelPatternData = new List<List<PatternData>>();
+
+            AssetReference[] assets;
+
+            for (int i = 0; i < levelPatterns.Count; i++)
+            {
+                assets = levelPatterns[i].GetComponentsInChildren<AssetReference>();
+                AddPattern(assets);
+            }
+        }
+        private void AddPattern(AssetReference[] assets)
+        {
+            List<PatternData> patterData = new List<PatternData>();
+            PatternData data;
+            if (assets != null)
+            {
+                for (int i = 0; i < assets.Length; i++)
+                {
+                    data = new PatternData();
+
+                    switch (assets[i].Asset)
+                    {
+                        case AssetRefType.None:
+                            break;
+                        case AssetRefType.Item:
+                            data.SetData(ItemType.Common, assets[i].Type, assets[i].GetPosition(), assets[i].GetRotation());
+
+                            break;
+                        case AssetRefType.Asteroid:
+                            data.SetData(EnemyType.Asteroid, assets[i].Type, assets[i].GetPosition(), assets[i].GetRotation());
+
+                            break;
+                        case AssetRefType.SpaceCraft:
+                            data.SetData(EnemyType.SpaceCraft, assets[i].Type, assets[i].GetPosition(), assets[i].GetRotation());
+                            break;
+                        case AssetRefType.WeaponItem:
+                            data.SetData(ItemType.Weapon, assets[i].Type, assets[i].GetPosition(), assets[i].GetRotation());
+                            break;
+                        case AssetRefType.Cherub:
+                            data.SetData(EnemyType.Cherub, assets[i].Type, assets[i].GetPosition(), assets[i].GetRotation());
+                            break;
+                        case AssetRefType.CharacterItem:
+                            data.SetData(ItemType.Character, assets[i].Type, assets[i].trayectoria, assets[i].GetPosition(), assets[i].GetRotation());
+                            break;
+
+                    }
+
+                   patterData.Add(data);
+                }
+            }
+            LevelPatternData.Add(patterData);
+        }
+    }
+}
+
